b93b5334ea72c94a79f1e5c7a3b542554b4214b5
sergeybabann <sergeybabann@gmail.com>
2025-05-16 12:57:52 +0300

    first commit

---
+++ 04_basics/11_variables_and_values/scripts/script.js
@@ -0,0 +1,13 @@
+let name = 'Антон'
+name = 'Сергей'
+
+let a = 1
+console.log(a)
+
+a = 'test'
+console.log(a)
+
+const age = 25
+
+console.log(name)
+console.log(age)
---
+++ 04_basics/12_arithmetic_operators/scripts/script.js
@@ -0,0 +1,23 @@
+// базовые арифметические операторы
+
+const width = 10
+const height = 5
+const space = width * height
+
+const newWidth = width - 4
+const newWidth2 = width + 4
+
+const division = newWidth / newWidth2
+
+const volume = 2 * 2 * 2 // вычисление кубического объема 1 вариант
+const volume2 = 2 ** 3 // вычисление кубического объема 2 вариант
+console.log(newWidth)
+console.log(newWidth2)
+console.log(division)
+console.log(volume)
+console.log(volume2)
+
+//-------------------- строки --------------------//
+const city = 'Москва'
+const street = 'Новослободская'
+console.log(city + ', ' + street + ' ' + 5)
