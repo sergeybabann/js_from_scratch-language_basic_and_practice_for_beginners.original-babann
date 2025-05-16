98048ac534cdfcbebf61fb77dbd85b972258d3b8
sergeybabann <sergeybabann@gmail.com>
2025-05-16 14:00:38 +0300

    04_основы / 14_Порядок операторов - complete!

---
+++ 04_basics/14_order_of_operators/scripts/script.js
@@ -0,0 +1,15 @@
+// порядок операторов
+
+const isSuited = 100 - 10 > 90 - 5
+console.log(isSuited)
+
+const a = 6 + 10 / 2 // 11
+const a2 = (6 + 10) / 2 // 8
+console.log(a)
+console.log(a2)
+
+let b
+let c
+c = b = 100 + 50
+console.log(c)
+console.log(b)
