a179b50afe30640b760ac888c869aa99ceaf5823
sergeybabann <sergeybabann@gmail.com>
2025-05-16 14:33:09 +0300

    04_основы / 15_типы данных - complete!

---
+++ 04_basics/15_data_types/scripts/script.js
@@ -0,0 +1,24 @@
+let a = 5
+let b = 5.6
+console.log(typeof a)
+console.log(typeof b)
+
+a = 'строка'
+console.log(typeof a)
+
+let isAdmin = false
+console.log(typeof isAdmin)
+
+isAdmin = a > 10
+console.log(typeof isAdmin)
+
+let c = undefined
+console.log(typeof c)
+c = 5
+console.log(typeof c)
+
+let d = null
+console.log(typeof d)
+console.log(d == null)
+console.log(typeof d == null)
+console.log(typeof (d == null))
