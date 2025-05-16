06ccc50d357554519c928db61742ca18d80e3edb
sergeybabann <sergeybabann@gmail.com>
2025-05-16 13:26:02 +0300

    04_основы / 13_операторы присваивания и сравнения - complete!

---
+++ 04_basics/13_assignment_and_comparison_operators/scripts/script.js
@@ -0,0 +1,20 @@
+// операторы присваивания
+
+let age = 18
+age = 18 + 5
+age += 2 // age = age + 2
+age -= 3 // age = age - 3
+age *= 2 // age = age * 2
+age /= 2 // age = age / 2
+
+age++ // age = age + 1
+age-- // age = age - 1
+console.log(age)
+
+// операторы сравнения
+const vasia = 20
+console.log(age > vasia) // больше
+console.log(age >= vasia) // больше либо равно
+console.log(age < vasia) // меньше
+console.log(age <= vasia) // меньше либо равно
+console.log(age == vasia) // равно
