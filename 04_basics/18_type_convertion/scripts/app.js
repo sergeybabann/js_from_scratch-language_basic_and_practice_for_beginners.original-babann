62aac2e27dc07302178132bbfdb366741e433fd1
sergeybabann <sergeybabann@gmail.com>
2025-05-17 22:39:21 +0300

    04_basic / 18_Преобразование типов - complete!

---
+++ 04_basics/18_type_convertion/scripts/app.js
@@ -0,0 +1,22 @@
+const age = '18'
+console.log(Number(age) + 5)
+console.log(age - 3)
+console.log(age / 3)
+console.log(age * 3)
+
+const userName = 'Вася'
+console.log(Number(userName) + 5)
+console.log(typeof NaN)
+
+console.log(String(4) + 7)
+console.log(Boolean(1))
+console.log(Boolean(0))
+console.log(Boolean(123))
+console.log(Boolean(-1))
+console.log(Boolean('текст'))
+console.log(Boolean(''))
+console.log(Boolean('') + 10)
+console.log(true + 2)
+
+const a = 2 + '10'
+console.log(a - 10)
